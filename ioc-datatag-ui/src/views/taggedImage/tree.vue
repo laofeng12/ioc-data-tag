d6a2f495bf1a4c400a9556246e807d6b1c263222
zhujianxiong <zhujx@alltosea.com>
2019-07-02 20:08:26 +0800

    #34 标签管理中创建标签页面开发

---
+++ ioc-datatag-ui/src/views/taggedImage/tree.vue
@@ -0,0 +1,28 @@
+<template>
+  <!--<div class="app-container">创建标签</div>-->
+  <div class="app-container">
+    <treeLabel></treeLabel>
+  </div>
+</template>
+
+<script>
+  import treeLabel from '@/components/treeLabel/treeLabel'
+
+  export default {
+    components: {
+      treeLabel
+    },
+    name: 'tree',
+    data() {
+      return {}
+    },
+    mounted() {
+
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
