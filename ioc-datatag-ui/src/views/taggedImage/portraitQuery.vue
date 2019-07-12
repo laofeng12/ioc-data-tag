48058ae68747d5843dc4e613c000596b579c3a22
zhujianxiong <zhujx@alltosea.com>
2019-07-12 18:55:09 +0800

    #37 数据标签画像查询页面

--- ioc-datatag-ui/src/views/taggedImage/portraitQuery.vue
+++ ioc-datatag-ui/src/views/taggedImage/portraitQuery.vue
@@ -1,13 +1,73 @@
 <template>
-    <div class="app-container">画像查询</div>
+  <div class="app-container">
+    <div v-show="query">
+      <div class="queryTitle">画像查询</div>
+      <div class="queryIt">
+        <el-input
+          class="zxinp moduleOne"
+          size="small"
+          placeholder="请输入内容"
+          prefix-icon="el-icon-search"
+          v-model="input2">
+        </el-input>
+        <el-button class="zxlistBtn" size="small" type="primary" @click="queryDetail">查询</el-button>
+      </div>
+    </div>
+    <queryImage v-show="queryImage"></queryImage>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "portraitQuery"
+  import {mapActions, mapState, mapGetters} from 'vuex'
+  import queryImage from '@/components/image/queryImage'
+
+  export default {
+    components: {
+      queryImage
+    },
+    name: "portraitQuery",
+    data() {
+      return {
+        input2: '',
+        query: true,
+        queryImage:false
+      }
+    },
+    methods: {
+      queryDetail(){
+        this.queryImage = true
+        this.query = false
+      }
+    },
+    created() {
+    },
+    computed: {},
+    watch: {},
+    mounted() {
     }
+  }
 </script>
 
 <style scoped>
+  .zxinp {
+    width: 500px;
+  }
 
+  .zxlistBtn {
+    margin-left: 10px;
+  }
+
+  .queryTitle {
+    font-size: 24px;
+    font-weight: 700;
+    color: #FF0000;
+    text-align: center;
+    margin-top: 194px;
+  }
+
+  .queryIt {
+    display: flex;
+    justify-content: center;
+    margin-top: 25px;
+  }
 </style>
