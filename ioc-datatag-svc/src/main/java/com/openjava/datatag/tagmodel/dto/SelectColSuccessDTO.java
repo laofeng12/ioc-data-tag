172dee0449b609d7cb62c700be40c4ebf0f1b80e
林成行 <linch@alltosea.com>
2019-07-30 15:07:26 +0800

    添加缺少的dto

---
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/tagmodel/dto/SelectColSuccessDTO.java
@@ -0,0 +1,10 @@
+package com.openjava.datatag.tagmodel.dto;
+
+import com.openjava.datatag.tagmanage.dto.BaseMessageDTO;
+import lombok.Data;
+
+@Data
+public class SelectColSuccessDTO extends BaseMessageDTO {
+    Long TaggingModelId;
+
+}
