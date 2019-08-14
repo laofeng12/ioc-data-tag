f62450ff4e7cbe7f6f69063e6976737d4f42437b
zhujianxiong <zhujx@alltosea.com>
2019-08-14 15:43:11 +0800

    #97 #80 画像查询的提示语修改

--- ioc-datatag-ui/src/views/taggedImage/portraitQuery.vue
+++ ioc-datatag-ui/src/views/taggedImage/portraitQuery.vue
@@ -6,7 +6,7 @@
         <el-input
           class="zxinp moduleOne"
           size="small"
-          placeholder="请输入内容"
+          placeholder="请输入需要查询的ID"
           prefix-icon="el-icon-search"
           v-model="input2">
         </el-input>
