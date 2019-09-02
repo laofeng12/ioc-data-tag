444a94a2bd11453e5f2e4964f5fbffaaa9042ec3
郑绵铠 <1003089887@qq.com>
2019-09-02 15:33:24 +0800

    #156 报错信息乱码问题

--- ioc-datatag-svc/src/main/java/com/openjava/datatag/tagmodel/service/DtTaggingModelServiceImpl.java
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/tagmodel/service/DtTaggingModelServiceImpl.java
@@ -469,7 +469,7 @@ public class DtTaggingModelServiceImpl implements DtTaggingModelService {
 	/**
 	 * 获取打标数据并根据规制自动打标（核心方法）
 	 */
-	public void calculation(DtTaggingModel tagModel){
+	public void calculation(DtTaggingModel tagModel) throws Exception{
 		Long taggingModelId = tagModel.getTaggingModelId();
 		List<DtSetCol> cols= dtSetColService.getByTaggingModelId(taggingModelId);
 		if (CollectionUtils.isEmpty(cols)){
@@ -549,7 +549,7 @@ public class DtTaggingModelServiceImpl implements DtTaggingModelService {
 			e.printStackTrace();
 			logger.info(e.getMessage());
 			DtTaggingErrorLog errorLog = new DtTaggingErrorLog();
-			errorLog.setErrorInfo(e.getMessage().getBytes());
+			errorLog.setErrorInfo(e.getMessage().getBytes("utf-8"));
 			errorLog.setErrorTime(new Date());
 			errorLog.setTaggingModelId(taggingModelId);
 			dtTaggingErrorLogService.doSave(errorLog);
