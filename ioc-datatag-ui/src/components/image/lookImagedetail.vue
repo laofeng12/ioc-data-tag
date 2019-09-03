0a41ed489a6c5797bd1269da4c2b2e150c60c9a7
zhujianxiong <zhujx@alltosea.com>
2019-09-03 10:42:32 +0800

    #80

--- ioc-datatag-ui/src/components/image/lookImagedetail.vue
+++ ioc-datatag-ui/src/components/image/lookImagedetail.vue
@@ -242,7 +242,10 @@
   .circularContent {
     margin-top: 120px;
     margin-bottom: 135px;
-    margin-left: 80px;
+    /*margin-left: 80px;*/
+    padding-left: 80px;
+    padding-right: 80px;
+    overflow-x: auto;
   }
 
   .peopleDetailid, .peopleDetail1, .peopleDetail2, .peopleDetail3, .peopleDetail4, .peopleDetail5, .peopleDetail6, .peopleDetail7, .peopleDetail8, .peopleDetail9, .peopleDetail10 {
