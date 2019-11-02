6dd5625456a1e51405ac3540fe39bd37e05eff52
郑绵铠 <1003089887@qq.com>
2019-11-02 17:39:35 +0800

    应前端要求修改返回的数据格式

--- ioc-datatag-svc/src/main/java/com/openjava/datatag/utils/tree/TagDTOTreeNodeShow2.java
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/utils/tree/TagDTOTreeNodeShow2.java
@@ -2,6 +2,7 @@ package com.openjava.datatag.utils.tree;
 
 import com.openjava.datatag.tagmanage.dto.DtTagDTO2;
 import lombok.Data;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +11,16 @@ import java.util.List;
 public class TagDTOTreeNodeShow2 {
     private String value;
     private String label;
-    List<TagDTOTreeNodeShow2> children = new ArrayList<>();
+    List<TagDTOTreeNodeShow2> children ;
     public TagDTOTreeNodeShow2(TagDTOTreeNode tree){
         this.value = tree.getTag2().getValue();
         this.label = tree.getTag2().getLabel();
-        children = new ArrayList<>();
-        for (TagDTOTreeNode ctree: tree.getChildrenNode()){
-            TagDTOTreeNodeShow2 ctreeShow = new TagDTOTreeNodeShow2(ctree);
-            children.add(ctreeShow);
+        if(CollectionUtils.isNotEmpty(tree.getChildrenNode())){
+            children = new ArrayList<>();
+            for (TagDTOTreeNode ctree: tree.getChildrenNode()){
+                TagDTOTreeNodeShow2 ctreeShow = new TagDTOTreeNodeShow2(ctree);
+                children.add(ctreeShow);
+            }
         }
     }
 }
