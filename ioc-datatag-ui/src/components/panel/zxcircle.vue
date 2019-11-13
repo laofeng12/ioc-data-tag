a960e0a6b51f71f8fd67c35cf3a0c4407cc31a1f
zhujianxiong <zhujx@alltosea.com>
2019-11-13 14:07:52 +0800

    #301 去掉提示

--- ioc-datatag-ui/src/components/panel/zxcircle.vue
+++ ioc-datatag-ui/src/components/panel/zxcircle.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="chart">
-    <div id ="myCircle" :style="conheight"></div>
+    <div id="myCircle" :style="conheight"></div>
   </div>
 </template>
 
 <script>
   import {mapActions, mapState, mapGetters} from 'vuex'
   import echarts from 'echarts'
+
   export default {
     name: 'zxcircle',
     data() {
       return {
-        arr:[],
-        conheight:{
-          height:''
+        arr: [],
+        conheight: {
+          height: ''
         }
       }
     },
@@ -21,13 +22,13 @@
       this.hadCircle()
     },
     methods: {
-      hadCircle(){
+      hadCircle() {
         // 基于准备好的dom，初始化echarts实例
         const myChart = echarts.init(document.getElementById('myCircle'))
         const option = {
           backgroundColor: '#fff',
-          tooltip: {},
-          animationDurationUpdate: function(idx) {
+          // tooltip: {},
+          animationDurationUpdate: function (idx) {
             // 越往后的数据延迟越大
             return idx * 100;
           },
@@ -56,64 +57,64 @@
           }]
         }
         myChart.setOption(option);
-        window.onresize = function(){
+        window.onresize = function () {
           myChart.resize();
         }
       },
-// 计算字符长度
-        textLength(str){
-        let len = 0;
-        for(let a of str){
-          if(/[\u4E00-\u9FA5]/.exec(a)){
-            len += 1;
-          }else{
-            len += 0.5;
+      // 计算字符长度
+      textLength(str) {
+        let len = 0;
+        for (let a of str) {
+          if (/[\u4E00-\u9FA5]/.exec(a)) {
+            len += 1;
+          } else {
+            len += 0.5;
           }
         }
-          if(len < 8 ){
-            const numOne = {
-              "name": str,
-              "value": 582,
-              "symbolSize": 80,
-              "draggable": true,
-              "itemStyle": {
-                "normal": {
-                  "borderColor": "#69B7FF",
-                  "borderWidth": 4,
-                  "color": "#69B7FF"
-                }
+        if (len < 8) {
+          const numOne = {
+            "name": str,
+            "value": 582,
+            "symbolSize": 80,
+            "draggable": true,
+            "itemStyle": {
+              "normal": {
+                "borderColor": "#69B7FF",
+                "borderWidth": 4,
+                "color": "#69B7FF"
               }
             }
-            this.arr.push(numOne)
-          }else{
-            const numTwo = {
-              "name": str,
-              "value": 1484,
-              "symbolSize": 100,
-              "draggable": true,
-              "itemStyle": {
-                "normal": {
-                  "borderColor": "#0486FE",
-                  "borderWidth": 4,
-                  "color": "#0486FE"
-                }
-              }
-            }
-            this.arr.push(numTwo)
           }
+          this.arr.push(numOne)
+        } else {
+          const numTwo = {
+            "name": str,
+            "value": 1484,
+            "symbolSize": 100,
+            "draggable": true,
+            "itemStyle": {
+              "normal": {
+                "borderColor": "#0486FE",
+                "borderWidth": 4,
+                "color": "#0486FE"
+              }
+            }
+          }
+          this.arr.push(numTwo)
+        }
         let obj = 0
-          this.arr.forEach(_item =>{
-            obj = obj + _item.symbolSize
-          })
-          if(obj < 100){
-            obj = 100
-          }
-          this.conheight.height = obj +'px';
-          return len;
+        this.arr.forEach(_item => {
+          obj = obj + _item.symbolSize
+        })
+        if (obj < 100) {
+          obj = 100
+        }
+        this.conheight.height = obj + 'px';
+        return len;
       },
     },
     created() {
-      this.listArr.lists.map(item =>{
+      this.listArr.lists.map(item => {
         this.textLength(item)
       })
     },
