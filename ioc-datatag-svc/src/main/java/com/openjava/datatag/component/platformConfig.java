7b7808792073e8c5a310368f7a860a942279a237
郑绵铠 <1003089887@qq.com>
2019-12-04 14:01:34 +0800

    #323 服务平台首页对接

---
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/component/platformConfig.java
@@ -0,0 +1,15 @@
+package com.openjava.datatag.component;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "platform")
+public class platformConfig {
+    private String baseUrl;
+    private String spUnifyWorkform;
+    private String finish;
+    private String spUnifyMsgNotice;
+}
