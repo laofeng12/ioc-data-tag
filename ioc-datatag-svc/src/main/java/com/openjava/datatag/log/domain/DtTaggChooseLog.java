214781f029dc5dc5869e8a6a28b3d00a2b007782
郑绵铠 <1003089887@qq.com>
2020-02-04 18:37:27 +0800

    #371 共享标签添加选用日志

--- ioc-datatag-svc/src/main/java/com/openjava/datatag/log/domain/DtTaggChooseLog.java
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/log/domain/DtTaggChooseLog.java
@@ -52,7 +52,11 @@ public class DtTaggChooseLog implements Persistable<Long>,Serializable {
 	@Max(9223372036854775806L)
 	@Column(name = "CHOOSE_USER")
 	private Long chooseUser;
-	
+
+	@ApiModelProperty("选用者姓名")
+	@Column(name = "CHOOSE_USER_NAME")
+	private String chooseUserName;
+
 	@ApiModelProperty("选用时间")
 	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
@@ -64,7 +68,14 @@ public class DtTaggChooseLog implements Persistable<Long>,Serializable {
 	@Length(min=0, max=48)
 	@Column(name = "CHOOSER_IP")
 	private String chooserIp;
-	
+
+	@ApiModelProperty("选用部门id")
+	@Column(name = "CHOOSE_ORG_ID")
+	private String chooseOrgid;
+
+	@ApiModelProperty("选用部门名称")
+	@Column(name = "CHOOSE_ORG_NAME")
+	private String chooseOrgNmae;
 	
 	@ApiModelProperty("是否新增")
 	@Transient
