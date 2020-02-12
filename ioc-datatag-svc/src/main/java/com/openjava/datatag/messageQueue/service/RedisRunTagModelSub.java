5861a7c7540fccad2482b8d193dcddd6c9bf571a
郑绵铠 <1003089887@qq.com>
2020-02-12 17:42:55 +0800

    #378

--- ioc-datatag-svc/src/main/java/com/openjava/datatag/messageQueue/service/RedisRunTagModelSub.java
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/messageQueue/service/RedisRunTagModelSub.java
@@ -5,6 +5,8 @@ import com.openjava.datatag.component.RedisMsg;
 import com.openjava.datatag.tagmodel.domain.DtTaggingModel;
 import com.openjava.datatag.tagmodel.service.DtTaggingModelService;
 import lombok.Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +19,12 @@ import javax.annotation.Resource;
  */
 @Service
 public class RedisRunTagModelSub implements RedisMsg {
-
+    Logger logger = LogManager.getLogger(getClass());
     @Value("${messageChannel.runNow}")
     private String runNow;
     @Resource
     private DtTaggingModelService dtTaggingModelService;
+
     /**
      * 接收消息的方法
      * @param message 订阅消息体
@@ -31,6 +34,7 @@ public class RedisRunTagModelSub implements RedisMsg {
         //注意通道调用的方法名要和RedisConfig2的listenerAdapter的MessageListenerAdapter参数2相同
         if ("true".equals(runNow)){
             DtTaggingModel model = JSONObject.parseObject(message, DtTaggingModel.class);
+            logger.info("立即执行，模型id："+model.getTaggingModelId());
             dtTaggingModelService.calculation(model);
         }
     }
