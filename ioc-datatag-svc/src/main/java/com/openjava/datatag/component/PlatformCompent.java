cd8e3e429c68675d52c964ee7c92493b7f876792
ccg <549958058@qq.com>
2020-03-24 15:13:11 +0800

    #366-打标待处理、已作废、完成、发起人错误bug修复

--- ioc-datatag-svc/src/main/java/com/openjava/datatag/component/PlatformCompent.java
+++ ioc-datatag-svc/src/main/java/com/openjava/datatag/component/PlatformCompent.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import org.apache.http.HttpResponse;
 import org.ljdp.common.http.HttpClientUtils;
 import org.ljdp.common.http.LjdpHttpClient;
-import org.omg.CORBA.PUBLIC_MEMBER;
+//import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -36,14 +36,14 @@ public class PlatformCompent {
      * 提交工单
      * @param businessId
      * @param userId 协作用户
-     * @param userAccount
-     * @param
+     * @param userAccount 接收人（协作用户)
+     * @param createUserId 工单发起人
      */
-    public void spUnifyWorkform(String businessId,String userId,String userAccount){
+    public void spUnifyWorkform(String businessId,String createUserId,String userId,String userAccount){
         String url = spUnifyWorkform;//提交工单url
         try {
             LjdpHttpClient client = new LjdpHttpClient();
-            client = initcClient(client,Long.valueOf(userId));
+            client = initcClient(client,Long.valueOf(createUserId));
             SpUnifyWorkformDTO spUnifyWorkformDTO = new SpUnifyWorkformDTO();
             spUnifyWorkformDTO.setJobPath(jobPath);
             spUnifyWorkformDTO.setBusinessId(businessId);
@@ -62,12 +62,13 @@ public class PlatformCompent {
      * 完成工单
      * @param businessId
      * @param userId
+     *  @param createUser  工单发起人
      */
-    public void finish(String businessId,String userId){
+    public void finish(String businessId,String userId,String createUser){
         String url = String.format(finish,1,businessId,userId);//完成工单url
         try {
             LjdpHttpClient client = new LjdpHttpClient();
-            client = initcClient(client,Long.valueOf(userId));
+            client = initcClient(client,Long.valueOf(createUser));
             HttpResponse resp = client.postJSON(url, "");
             String jsontext = HttpClientUtils.getContentString(resp.getEntity(), "utf-8");
             System.out.println(jsontext);
@@ -80,12 +81,13 @@ public class PlatformCompent {
      * 取消工单
      * @param businessId
      * @param userId
+     * @param createUser  工单发起人
      */
-    public void cancel(String businessId,String userId){
+    public void cancel(String businessId,String userId,String createUser){
         String url = String.format(cancel,1,businessId,userId);//取消工单url
         try {
             LjdpHttpClient client = new LjdpHttpClient();
-            client = initcClient(client,Long.valueOf(userId));
+            client = initcClient(client,Long.valueOf(createUser));
             HttpResponse resp = client.postJSON(url,"");
             String jsontext = HttpClientUtils.getContentString(resp.getEntity(), "utf-8");
             System.out.println(jsontext);
@@ -115,6 +117,9 @@ public class PlatformCompent {
             e.printStackTrace();
         }
     }
+    /**
+     * @param userid 工单发起者
+     */
     public LjdpHttpClient initcClient(LjdpHttpClient client,Long userid){
         String  token= tokenGenerator.createToken(userid);//模型创建者才有权限调用数据集接口
         client.setHeader("authority-token",token);//
